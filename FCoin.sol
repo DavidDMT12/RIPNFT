0363b605bf2b054c9b609a2bc0c87e4087476c4e
deliveredmetal <118897748+DavidDMT12@users.noreply.github.com>
2023-06-13 08:51:30 +0200

    Update FCoin.sol

--- FCoin.sol
+++ FCoin.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -7,34 +8,41 @@ import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract FCOIN is ERC20, ERC20Burnable, Ownable, ERC20Permit {
 
-    constructor(address _daiTokenAddress) ERC20("FCOIN", "FCN") ERC20Permit("FCOIN") {
+    constructor(address _daiTokenAddress, address _minterContract) ERC20("FCOIN", "FCN") ERC20Permit("FCOIN") {
         daiTokenAddress = _daiTokenAddress;
+        admin = msg.sender;
+        minterContract = _minterContract;
     }
 
+    ERC20 daiToken = ERC20(daiTokenAddress);
+
     //Variables
-    uint256 public admin;
+    address public minterContract;
+    address public admin;
     uint256 public adminBalance;
-    mapping(address => uint256) public payedRespects;
-    uint256 conversion = 5;
+    uint256 conversion = 1;
     address public daiTokenAddress;  // Address of the DAI token contract
     uint256 eventCounter;
 
     struct Event {
         string Name;
-        address Owner;
+        address Creator;
         uint256 Duration;
         uint256 StartTime;
         bool HasEnded;
-        string NFT;
+        string NFTURI;
         uint256 FCoins;
-        address[] Supporters; 
+        address[] Supporters;
+        uint256 MinimumTip; 
     }
 
     mapping(uint256 => Event) public events; // tracks event by ID
 
+
     //Events
     event BuyFCoin(address payer, uint256 amount);
-    event EventStarted(uint256 ID, address indexed creator, uint duration);
+    event EventStarted(uint256 ID, address indexed creator, uint256 duration);
+    event EventEnded(uint256 ID, address indexed creator, uint256 amount);
 
     //Override functions
     function mint(address to, uint256 amount) public onlyOwner {
@@ -52,7 +60,6 @@ contract FCOIN is ERC20, ERC20Burnable, Ownable, ERC20Permit {
 
     //Main Logic
     function getFcoins(uint256 _amount) external {
-        ERC20 daiToken = ERC20(daiTokenAddress);
         uint256 DAI = _amount * conversion;
         // Transfer DAI tokens from the payer to this contract
         bool success = daiToken.transferFrom(msg.sender, address(this), DAI);
@@ -61,22 +68,45 @@ contract FCOIN is ERC20, ERC20Burnable, Ownable, ERC20Permit {
         emit BuyFCoin(msg.sender, _amount);
     }
     
-    function startEvent(string calldata _Name, string calldata _NFT, uint256 _minutes) external {
+    function startEvent(string calldata _Name, string calldata _NFTURI, uint256 _minutes, uint256 _MinimumTip) external {
         require(_minutes <= 10080, "Maximum duration is 1 week");
+        require(_MinimumTip >0, "Amount to participate must be larger than 0");
         uint256 _thisEvent = eventCounter;
         events[_thisEvent].Name = _Name;
-        events[_thisEvent].NFT = _NFT;
+        events[_thisEvent].NFTURI = _NFTURI;
         events[_thisEvent].Duration = _minutes;
-        events[_thisEvent].Owner = msg.sender;
+        events[_thisEvent].Creator = msg.sender;
         events[_thisEvent].StartTime = block.timestamp;
+        events[_thisEvent].MinimumTip = _MinimumTip;
         eventCounter = eventCounter + 1;
         emit EventStarted(_thisEvent, msg.sender, _minutes);
     }
 
     function payRespects(uint256 _eventID, uint256 _amount) external {
         require(block.timestamp < events[_eventID].StartTime + (events[_eventID].Duration * 60), "The time to pay respects has ended");
-        require(HasEnded == false, "Event has ended");
-        transfer()
+        require(events[_eventID].HasEnded == false, "Event has ended");
+        require(_amount >=  events[_eventID].MinimumTip, "Tip is not large enough");
+        transfer(address(this), _amount);
+        events[_eventID].FCoins = events[_eventID].FCoins + _amount;
+        events[_eventID].Supporters.push(msg.sender);
+    }
+
+    function endEvent(uint256 _eventID) external {
+        require(block.timestamp >= events[_eventID].StartTime + (events[_eventID].Duration * 60), "The event is still ongoing");
+        require(events[_eventID].HasEnded == false, "Event has ended");
+        events[_eventID].HasEnded == true;
+        uint256 DAI = (events[_eventID].FCoins  * 97) / conversion / 100;
+        adminBalance = DAI - events[_eventID].FCoins;
+        burn(events[_eventID].FCoins);
+        bool success = daiToken.transferFrom(address(this), msg.sender, DAI);
+        require(success, "Failed to send DAI payment");
+        emit EventEnded(_eventID ,events[_eventID].Creator, events[_eventID].FCoins);
+    }
+
+    function redeem() external onlyOwner {
+        uint256 DAI = adminBalance;
+        bool success = daiToken.transferFrom(address(this), msg.sender, DAI);
+        require(success, "Failed to send DAI payment");
     }
 
 }
